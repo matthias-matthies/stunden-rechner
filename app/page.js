501a13227e81f6223238cf13b16434b78dbd55ae
matthias-matthies <matthias.matthies.business@gmail.com>
2023-09-29 16:27:02 +0200

    Function setup

--- app/page.js
+++ app/page.js
@@ -1,113 +1,97 @@
-import Image from 'next/image'
+"use client"
+
+import {useEffect, useState} from 'react'
+
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const calcTime = (start, end) => {
+        const startHour = start.slice(0, start.indexOf(":"))
+        const startMinute = start.slice(start.indexOf(":")+1)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        const endHour = end.slice(0, end.indexOf(":"))
+        const endMinute = end.slice(end.indexOf(":")+1)
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        const workHour = endHour - startHour
+        const workMinute = workHour * 60 + (endMinute - startMinute)
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        console.log(workMinute)
+        return workMinute
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    const [sum, setSum] = useState(0)
+    const [times, setTimes] = useState([])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    useEffect(() => {
+        setSum(
+            times
+                .map(time => time.work)
+                .reduce((acc, curr) => {
+                    return acc + curr
+                }, 0)
+        )
+    }, [times])
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        const timeObject = {
+            start: formJson.start,
+            end: formJson.end,
+            day: formJson.day.slice(formJson.day.lastIndexOf("-")+1),
+            work: calcTime(formJson.start, formJson.end)
+        }
+
+        setTimes([...times, timeObject])
+    }
+
+    function removeTime(index) {
+        setTimes([...times.slice(0, index), ...times.slice(index + 1)]);
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="day">
+                        Tag
+                        <input name="day" id={`day`} type="date"  className={`p-2 border`}/>
+                    </label>
+                    <br />
+                    <label htmlFor="start" className={`my-4`}>
+                        Von
+                        <input name="start" id={`start`} type="time" className={`p-2 border`}/>
+                    </label>
+                    <br />
+                    <label htmlFor="end" className={`p-4`}>
+                        Bis
+                        <input name="end" id={`end`} type="time" className={`p-2 border`}/>
+                    </label>
+                    <br />
+                    <input type="submit" value={`Hinzufügen`} className={`p-2 border`}/>
+                </form>
+            </div>
+            <div>
+                Display
+                Sum: {sum} Minuten oder {sum/60} Stunden (Dezimal)
+                <br />
+                Rechne also {sum/60} * Stundenlohn
+            </div>
+            <div>
+                <h3>Zeiten</h3>
+                <ul>
+                    {times.map((time, index) => {
+                        return (
+                            <li key={index} className={`font-mono my-2`}>
+                                Tag {time.day.length == 2 ? time.day : "0"+time.day}: {time.start.length === 5 ? time.start :"0"+time.start} - {time.end.length === 5 ? time.end :"0"+time.end} - Arbeitszeit in Minuten: {time.work}
+                                <button className={`p-2 border ml-2`} onClick={() => removeTime(index)}>Löschen</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </main>
+    )
 }
